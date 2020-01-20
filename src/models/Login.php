8b091bec85f79490bebb8cd277686c7be23524c8
João Victor Morais <jovic.morais@unifesspa.edu.br>
2020-01-20 17:15:40 -0300

    Começo do projeto final

--- src/models/Login.php
+++ src/models/Login.php
@@ -5,6 +5,11 @@ class Login extends Model {
 
     public function checkLogin(){
         $user = User::getOne(['email' => $this->email]);
-        
+        if ($user) {
+            if (password_verify($this->password, $user->password)) {
+                return $user;
+            }
+        }
+        throw new Exception();
     }
 }
