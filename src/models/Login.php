93d6d94bb92ff8ac6c415b624b601554afd4c663
João Victor Morais <jovic.morais@unifesspa.edu.br>
2020-01-20 16:26:50 -0300

    Começo do projeto final

---
+++ src/models/Login.php
@@ -0,0 +1,10 @@
+<?php
+require_once(realpath(MODEL_PATH . '/User.php'));
+
+class Login extends Model {
+
+    public function checkLogin(){
+        $user = User::getOne(['email' => $this->email]);
+        
+    }
+}
