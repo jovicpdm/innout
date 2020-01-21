91f62f00e2cd301a64c60030a43d58290e05eeea
João Victor Morais <jovic.morais@unifesspa.edu.br>
2020-01-21 09:51:27 -0300

    Tá funcionando até esse ponto

--- src/controllers/login.php
+++ src/controllers/login.php
@@ -6,8 +6,8 @@ if(count($_POST) > 0) {
     try {
         $user =$login->checkLogin();
         echo "Usuário {$user->name} logado";
-    } catch (Exception $e){
-        echo "Falha no Login :(";
+    } catch (AppException $e){
+        echo $e->getMessage();
     }
 }
 
