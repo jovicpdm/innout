86e83fe188aa61462117b0ede9a57bb73229a0e4
João Victor Morais <jovic.morais@unifesspa.edu.br>
2020-01-22 08:59:36 -0300

    Funcionando até aqui

--- src/config/loader.php
+++ src/config/loader.php
@@ -3,6 +3,7 @@
 function loadModel($modelName){
     require_once(MODEL_PATH . "/{$modelName}.php");
 }
+
 function loadView($viewName, $params = array()){
 
     if (count($params) > 0) {
@@ -13,5 +14,17 @@ function loadView($viewName, $params = array()){
         }
     }
 
+    require_once(VIEW_PATH . "/{$viewName}.php");
+}
+
+function loadTemplateView($viewName, $params = array()){
+
+    if (count($params) > 0) {
+        foreach ($params as $key => $value) {
+            if (strlen($key) > 0) {
+                ${$key} = $value;
+            }
+        }
+    }
     require_once(VIEW_PATH . "/{$viewName}.php");
 }
