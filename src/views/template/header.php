1796dff155bd4a116900e32f4aed14dc64d8a393
João Victor Morais <jovic.morais@unifesspa.edu.br>
2020-01-28 10:52:04 -0300

    AAAAH

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -10,10 +11,11 @@
     <link rel="stylesheet" href="assets/css/template.css">
     <title>In N' Out</title>
 </head>
+
 <body>
     <header class="header">
         <div class="logo">
-        <i class="icofont-travelling mr-2"></i>
+            <i class="icofont-travelling mr-2"></i>
             <span class="font-weight-light">In</span>
             <span class="font-weight-bold mx-2">N'</span>
             <span class="font-weight-light">Out</span>
@@ -25,7 +27,11 @@
         <div class="spacer"></div>
         <div class="dropdown">
             <div class="dropdown-button">
-                <span class="ml-3">Usuário Mock</span>
+                <img class="avatar" src="<?= "http://www.gravatar.com/avatar.php?gravatar_id="
+                                                . md5(strtolower(trim($_SESSION['user']->email))) ?>" alt="user">
+                <span class="ml-3">
+                    <?= $_SESSION['user']->name ?>
+                </span>
                 <i class="icofont-simple-down mx-2"></i>
             </div>
             <div class="dropdown-content">
