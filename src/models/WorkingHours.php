b37cdccfd7887049f7356b0d322d9b45fe710195
João Victor Morais <jovic.morais@unifesspa.edu.br>
2020-01-30 11:57:22 -0300

    AAAAH

--- src/models/WorkingHours.php
+++ src/models/WorkingHours.php
@@ -54,38 +54,37 @@ class WorkingHours extends Model{
         $part1 = new DateInterval('PT0S');
         $part2 = new DateInterval('PT0S');
 
-        if ($t1) $part1 = $t1->diff(new DateTime());
-        if ($t2) $part1 = $t1->diff($t2);
-        if ($t3) $part2 = $t3->diff(new DateTime());
-        if ($t4) $part2 = $t3->diff($t4);
+        if($t1) $part1 = $t1->diff(new DateTime());
+        if($t2) $part1 = $t1->diff($t2);
+        if($t3) $part2 = $t3->diff(new DateTime());
+        if($t4) $part2 = $t3->diff($t4);
 
         return sumIntervals($part1, $part2);
     }
 
-    function getLunchInterval(){
+    function getLunchInterval() {
         [, $t2, $t3,] = $this->getTimes();
-        $breakInterval = new DateInterval('PT0S');
+        $lunchInterval = new DateInterval('PT0S');
 
-        if ($t2) $breakInterval= $t2->diff(new DateTime());
-        if ($t3) $breakInterval = $t2->diff($t3);
+        if($t2) $lunchInterval = $t2->diff(new DateTime());
+        if($t3) $lunchInterval = $t2->diff($t3);
 
-        return $breakInterval;
+        return $lunchInterval;
     }
 
-    function getExitTime(){
-        [$t1,,, $t4] = $this->getTimes();
-        $workDay = DateInterval::createFromDateString('8 hours');
-//        $defaultBreakInterval = DateInterval::createFromDateString('1 hour');
 
-        if (!$t1) {
-            return (new DateTimeImmutable())->add($workDay);//->add($defaultBreakInterval);
-        } elseif ($t4) {
+    function getExitTime() {
+        [$t1,,, $t4] = $this->getTimes();
+        $workday = DateInterval::createFromDateString('8 hours');
+
+        if(!$t1) {
+            return (new DateTimeImmutable())->add($workday);
+        } elseif($t4) {
             return $t4;
         } else {
-            $total = sumIntervals($workDay, $this->getLunchInterval());
+            $total = sumIntervals($workday, $this->getLunchInterval());
             return $t1->add($total);
         }
-
     }
 
     function getTimes() {
